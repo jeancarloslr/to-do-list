63a79911ebe6761e0b8372e6a35da8d31568228a
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-07-31 22:37:01 -0300

    ShowValues()

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-const localStorageName = 'to-do-list';
 btn = document.getElementById('btn-ntask');
-
 btn.addEventListener('click', newTask);
+let values = [];
 
 function newTask(){
 
-let impt = document.getElementById('impt-new-task');
-let list = document.getElementById('to-do-list');
+ let impt = document.getElementById('impt-new-task');
 
     //validation
     if(!impt.value){
         alert('Nenhuma task foi inserida.');
     }
+
     //else if()
 
     else{
 
-        //incremeent to LocalStorage.
-        let values = JSON.parse(localStorage.getItem(localStorageName));
-
         values.push({
             name: impt.value
         })
+        impt.value = '';
+        
+        ShowValues()
+        console.log(values);
+        
 
     }
+
+    }
+
+    function ShowValues(){
+
+        list = document.getElementById('to-do-list')
+        list.innerHTML = '';
+    
+        
+        for(let i = 0; i <= values.length; i++){ //percorrer all elementos(array)
+
+            list.innerHTML += `<li>${values[i]['name']}</li> <button id='btn-ok' onclick = "removeItem(${values[1]['name']})">ok</button>`
+        }
+
+    }
+
+    function removeItem(data){
+        console.log(data);
 
     }
 
