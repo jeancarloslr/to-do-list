33b906c82691490bc8e673e2cc12ed143b4cd55e
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-08-12 16:03:39 -0300

    border color

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function newTask(){
 
     else if(inLista(impt.value, values)){
 
+        impt.style.borderColor = "red";
         alert('digite uma tarefa que não esteja na lista');
 
     }
@@ -40,6 +41,7 @@ function newTask(){
         impt.focus();
         
         ShowValues()
+
     }
 
     }
@@ -49,7 +51,7 @@ function newTask(){
         list = document.getElementById('to-do-list')
         list.innerHTML = '';
         
-        for(let pos in values){ //percorrer all elementos(array)
+        for(let pos in values){ 
 
             list.innerHTML += `<li>${values[pos]} <button id='btn' onclick='removeItem("${values[pos]}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
             <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
@@ -57,7 +59,7 @@ function newTask(){
 
         }
 
-        console.log(values);
+       
     }
 
     function removeItem(index){
