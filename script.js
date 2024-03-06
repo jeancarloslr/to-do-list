9be7ec214cd56aadc70447417ad2df1aaa97fbf8
Jean Carlos <jeanxh4@gmail.com>
2024-03-06 15:34:05 -0300

    Implementando o localStorage.

--- script.js
+++ script.js
@@ -1,72 +1,22 @@
-btn = document.getElementById('btn-ntask');
-btn.addEventListener('click', newTask);
-let values = [];
+const btnAddTask = document.querySelector('#btn-ntask');
+const task = document.querySelector('#impt-new-task');
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let lista = document.querySelector('#to-do-list');
 
-function newTask(){
-
- let impt = document.getElementById('impt-new-task');
- impt.style.borderColor = '';
-
- const tarefa = {
-    descricao: impt.value,
- }
-
- values.push(tarefa);
- localStorage.setItem('tarefas', JSON.stringify(values));
- console.log(values)
-    function inLista(imptvalue, valores){
-
-        if(valores.indexOf(imptvalue) != -1){
-
-            return true;
-
-        }else{
-            return false;
-        }
-
-    }  
-
-    //validation
-    if(!impt.value){
-
-        impt.style.borderColor = "red";
-        alert('insira alguma task.')
+function createTask(tarefa){
+    const li = document.createElement('li');
+    li.classList.add('liTarefa');
+    const paragrafo = document.createElement('p');
+    paragrafo.append(tarefa.descricao);
+    li.append(paragrafo);
+    return li;
+}
+btnAddTask.addEventListener('click', () =>{
+    const tarefa = {
+        descricao: task.value
     }
-    else if(inLista(impt.value, values)){
-
-        impt.style.borderColor = "red";
-        alert('digite uma tarefa que não esteja na lista');
-    }
-    else{
-        values.push(impt.value);
-        impt.value = '';
-        impt.focus();
-        
-        ShowValues()
-    }
-    }
-    function ShowValues(){
-
-        list = document.getElementById('to-do-list')
-        list.innerHTML = '';
-        
-        for(let pos in values){ 
-
-            list.innerHTML += `<li>${values[pos]} <button id='btn' onclick='removeItem("${values[pos]}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-          </svg></button></li>`
-
-        }
-
-    }
-
-    function removeItem(index){
-        
-      values.splice(index, 1)
-      ShowValues(); 
-
-      }
-    
-
-  
-
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    let elementoTarefa = createTask(tarefa);
+    lista.append(elementoTarefa);
+});
