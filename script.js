d935d49212e9af7a9da5b6c270a5e3db623ddfa1
Jean Carlos <jeanxh4@gmail.com>
2024-03-07 15:48:36 -0300

    button completed

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
 const btnAddTask = document.querySelector('#btn-ntask');
 const task = document.querySelector('#impt-new-task');
-let tarefas = JSON.parse(localStorage.getItem('tasks')) || [];
+let tarefas = JSON.parse(localStorage.getItem('atividades')) || [];
 let lista = document.querySelector('#to-do-list');
 
-function createTask(tarefa){
+function atualizarTask(){
+    localStorage.setItem('atividades', JSON.stringify(tarefas));
+}
+
+function createTask(tarefa) {
+
     const li = document.createElement('li');
     li.classList.add('liTarefa');
+    
     const paragrafo = document.createElement('p');
+    const circle = document.createElement('button');
+    circle.classList.add('btn');
+
+    const iconDone = document.createElement('img');
+    iconDone.setAttribute('src', 'img/done-icon.svg');
+    iconDone.classList.add('iconDone');
+    circle.append(iconDone);
+
     paragrafo.append(tarefa.descricao);
     li.append(paragrafo);
+    li.append(circle);
+
+    if(tarefa.completa){
+        return
+    }else{
+        circle.addEventListener('click', () => {
+            tarefa.completa = true;
+            li.classList.add('completed')
+            atualizarTask()
+        });
+    }
+   
     return li;
 }
 
+
 btnAddTask.addEventListener('click', () =>{
-    const tarefa = {
-        descricao: task.value
+    if(!task.value){
+        return;
+    }else{
+        const tarefa = {
+            descricao: task.value
+        }
+        tarefas.push(tarefa);
+        atualizarTask()
+        let elementoTarefa = createTask(tarefa);
+        lista.append(elementoTarefa);
     }
-    tarefas.push(tarefa);
-    localStorage.setItem('tasks', JSON.stringify(tarefas));
-    let elementoTarefa = createTask(tarefa);
-    lista.append(elementoTarefa);
 });
 
 tarefas.forEach(tarefa => {
