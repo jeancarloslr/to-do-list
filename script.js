824648db84a4a4af073f95dd4ed9cd4ddcd987ef
Jean Carlos <jeanxh4@gmail.com>
2024-03-07 12:49:41 -0300

    mantendo dados após refresh(localStorage)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnAddTask = document.querySelector('#btn-ntask');
 const task = document.querySelector('#impt-new-task');
-let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefas = JSON.parse(localStorage.getItem('tasks')) || [];
 let lista = document.querySelector('#to-do-list');
 
 function createTask(tarefa){
@@ -11,12 +11,18 @@ function createTask(tarefa){
     li.append(paragrafo);
     return li;
 }
+
 btnAddTask.addEventListener('click', () =>{
     const tarefa = {
         descricao: task.value
     }
     tarefas.push(tarefa);
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    localStorage.setItem('tasks', JSON.stringify(tarefas));
     let elementoTarefa = createTask(tarefa);
     lista.append(elementoTarefa);
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTask = createTask(tarefa);
+    lista.append(elementoTask);
 });
