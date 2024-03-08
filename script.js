7d66d9d99cc1d25577296c913270b309428eed1c
Jean Carlos <jeanxh4@gmail.com>
2024-03-08 14:34:05 -0300

    tarefas completas no localStorage

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btnAddTask = document.querySelector('#btn-ntask');
 const task = document.querySelector('#impt-new-task');
-let tarefas = JSON.parse(localStorage.getItem('atividades')) || [];
 let lista = document.querySelector('#to-do-list');
+let tarefas = JSON.parse(localStorage.getItem('atividades')) || []
+let buttonDisabled = null;
 
 function atualizarTask(){
     localStorage.setItem('atividades', JSON.stringify(tarefas));
@@ -13,9 +14,10 @@ function createTask(tarefa) {
     li.classList.add('liTarefa');
     
     const paragrafo = document.createElement('p');
+
     const circle = document.createElement('button');
     circle.classList.add('btn');
-
+    
     const iconDone = document.createElement('img');
     iconDone.setAttribute('src', 'img/done-icon.svg');
     iconDone.classList.add('iconDone');
@@ -26,18 +28,19 @@ function createTask(tarefa) {
     li.append(circle);
 
     if(tarefa.completa){
-        return
+        circle.setAttribute('disabled', 'disabled');
+        circle.classList.add("btnDisabled")
+        li.classList.add('completed');
     }else{
-        circle.addEventListener('click', () => {
+        circle.addEventListener('click', () =>{
             tarefa.completa = true;
-            li.classList.add('completed')
+            li.classList.add('completed');
+            circle.classList.add("btnDisabled")
             atualizarTask()
-        });
+        })
     }
-   
     return li;
 }
-
 
 btnAddTask.addEventListener('click', () =>{
     if(!task.value){
@@ -57,3 +60,4 @@ tarefas.forEach(tarefa => {
     const elementoTask = createTask(tarefa);
     lista.append(elementoTask);
 });
+
